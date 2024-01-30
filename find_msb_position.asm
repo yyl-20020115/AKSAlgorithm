dac0ea7c83fdfe55e56863704a990e8f3dba3b32
Yilin Yang <yyl_20050115@hotmail.com>
2024-01-30 14:01:23 +0800

    fx

--- find_msb_position.asm
+++ find_msb_position.asm
@@ -1,14 +1,14 @@
-global find_msb_position
+global		find_msb_position
 ; Input
-;   rcx - uint64 to find msb
+;   rcx		uint64 to find msb
 ; Output
-;   rax - msb position
+;   rax		msb position, or -1 if 0
 find_msb_position:
-	test rcx, rcx
-	jz is_zero
-	bsr rax, rcx
+	test	rcx, rcx
+	jz		is_zero
+	bsr		rax, rcx
 	ret
 is_zero:
-	xor rax, rax
-	not rax
+	xor		rax, rax
+	not		rax
 	ret
